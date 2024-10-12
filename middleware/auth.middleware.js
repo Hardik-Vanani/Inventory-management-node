71b9bba6d3b14ee2b2a22e913220d7d3de2e4bb4
Hardik <vananihardik8553@gmail.com>
2024-10-12 18:27:04 +0530

    Update auth.middleware.js

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -19,7 +19,6 @@ const auth = (req, res, next) => {
             req.user = decoded;
             req.token = token;
             
-            // Go to next task
             next();
         });
     } catch (error) {
