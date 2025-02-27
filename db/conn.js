3cb588bfebbca80bd854ecc8338012d4e50eb4dc
Hardik-Vanani <vananihardik8553@gmail.com>
2025-02-27 10:04:13 +0530

    Add env

--- db/conn.js
+++ db/conn.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose")
-mongoose.connect("mongodb://127.0.0.1/inventoryDB")
+require("dotenv").config()
+
+mongoose.connect(process.env.MONGO_URL)
     .then(
-        () => { 
+        () => {
             // console.log("connection successful")
-         }).catch((err => {
-        console.log(err)
-    }))
+        }).catch((err => {
+            console.log(err)
+        }))
