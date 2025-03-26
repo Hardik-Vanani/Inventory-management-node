419f7cb7d26c215fbfc56349b15c09cb75cbb542
Hardik-Vanani <vananihardik8553@gmail.com>
2025-03-26 13:37:51 +0530

    change connection file

--- config/db.conn.js
+++ config/db.conn.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose
-    .connect(process.env.MONGO_URL)
+    .connect(process.env.MONGO_URL, {
+        serverSelectionTimeoutMS: 50000, // 50 seconds timeout
+        socketTimeoutMS: 45000, // 45 seconds socket timeout
+    })
     .then(() => {
         console.log("✅ MongoDB connected!");
     })
     .catch((err) => {
-        console.log(err);
-
+        console.error("❌ MongoDB Connection Error:", err);
+        process.exit(1);
     });
